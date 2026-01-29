c6eb5229e66d80760717cf5d0ad09235e2b93d7f
Daniil <dshaulov@outlook.com>
2026-01-29 20:58:29 +0100

    Initialzie spring application

---
+++ src/main/java/io/github/dashdothub/rasptime_backend/RasptimeBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.dashdothub.rasptime_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RasptimeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RasptimeBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dashdothub/rasptime_backend/RasptimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dashdothub.rasptime_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RasptimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
